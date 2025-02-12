7e37b7a5c116f0cefb8370f54680a275102fe69a
Vaibhav Damdar <damdarv24@gmail.com>
2025-02-12 14:01:30 +0530

    Update 02_function.js

--- 04_Functions/02_function.js
+++ 04_Functions/02_function.js
@@ -1,65 +1,111 @@
 
-// function calculationCartPrice(num1){
-//     return num1
-// }
-// console.log(calculationCartPrice(2));
+// Fuction Declaration
 
-// If multiple inputs are comming from the user then we use rest operator (...). It is used to bundled the multiple input into single array
-
-
-function calculationCartPrice(...num1){
-    return num1
+function sayMyName () {  // function definition
+    console.log("Hello Vaibhav"); 
 }
-console.log(calculationCartPrice(200, 400, 500))
+
+sayMyName();  // Calling function
+
+function addTwoNumbers (number1, number2){ 
+
+result = number1 + number2
+return number3
+
+//            or
+
+    return number1 + number2
+}
+const result = addTwoNumbers(3, 5); //
+console.log(result);
+
+
+
+function loginUserMessage (username) {
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Vaibhav"));
+
+// If user not enterd his name or empty string then
+
+console.log(loginUserMessage(""));  // output will be  "just logged in"
+
+// If user doesn't pass any value then
+
+console.log(loginUserMessage()); // ==> output will be "undefined just logged in"
+
+
+//                or 
+
+
+function user (username) {         // if user did not entered any username then output will be please enter a username
+    if (username === undefined){ // here is the condition to check the entered value is equal to undefined or not if yes it will enter the if block
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in` 
+}
+ console.log(user());
+
 
 //                or
 
-function calculationCartPrice(val1, val2, ...num1){
-    return num1
+
+function user (username) {
+    if (!username){                // here we use ! operator to coonvert boolean value true ==> false, false ==> true
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
 }
-console.log(calculationCartPrice(200, 400, 500, 2000));
-// in the above case the val1 store 200, val2 store 400 and remaining parameter will store by the ...num 
+console.log(user("Vaibhav"));
 
+//                or 
 
-//                or
-
-
-const user = {
-    name : "Vaibhav",
-    price : 499
+function user (username = "Mr. VD") {    // here we pass the default value, if user not entered any value then default value get printed
+    if (!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
 }
-
-function getObjectValues (Anyobject){
-    console.log(`Hello user, The name is ${user.name} and Price is ${user.price}`)
-}
-getObjectValues(user)
-
-// we can pass object directly without declaring as parameter 
-
-function getObjectValues (Anyobject){
-    console.log(`Hello user, The name is ${Anyobject.name} and Price is ${Anyobject.price}`)
-}
-
-getObjectValues({
-    name : "skybag",
-    price : 699
-})
+console.log(user("Vaibhav"));
 
 
-// And also we can Pass Array as parameter to the function
 
-const array = [200, 400, 500, 600]
 
-function getArrayvalues (anyObject){
-    return anyObject[1]     // here we will return the value which is present at the index 1
-}
 
-console.log(getArrayvalues(array));
 
-// We can pass the array as parameter directly to the function without declaring separately
 
-function getArray (anyArray) {
-    return anyArray[5]
-}
+/* Function Notes  */
 
-console.log(getArray([100, 200, 300, 400, 500, 600]));
+/*
+1. Parameters definition ==>
+
+when we pass the values in the definition of the function its known as parameter 
+or
+parameters are the variables listed in the function definition
+
+2. Arguments Definition ==>
+
+when we pass the value while calling the function known as arguments
+or
+Argumentd are the actual values passed to a function when it is invoked
+
+3. In fuction after return statement fuction will not get executed anything.
+
+In JavaScript empty string and undefined = false value
+
+! operator is used to convert the boolean value means if true ==> false,  false ==> true
+
+function user (username = "Mr. VD") { 
+==> here we pass the default value that means the username will never be = undefined
+
+if user will enter empty string then default value Mr.VD will get printed 
+
+and
+
+if user will enter the value then default value get overrided
+
+*/
